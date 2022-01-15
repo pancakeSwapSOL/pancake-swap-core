b6b8aa8a2ca9a082c27ca8742944b68b9f9ccba4
PancakeSwapSolidity <97768009+pancakeSwapSOL@users.noreply.github.com>
2022-01-14 21:41:01 -0500

    Update PancakeERC20.sol

--- contracts/PancakeERC20.sol
+++ contracts/PancakeERC20.sol
@@ -22,18 +22,6 @@ contract PancakeERC20 is IPancakeERC20 {
 
     constructor() public {
         uint chainId;
-        assembly {
-            //chainId := chainid
-        }
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(
-                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes(name)),
-                keccak256(bytes('1')),
-                chainId,
-                address(this)
-            )
-        );
     }
 
     function _mint(address to, uint value) internal {
